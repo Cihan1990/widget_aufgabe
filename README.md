607fc384ad6eae0790e7dcca2b3884eaab4dfc17
Cihan1990 <cihan.52@hotmail.de>
2024-05-22 11:02:31 +0200

    Tasksheet erstellt

---
+++ README.md
@@ -0,0 +1,3 @@
+# widget_aufgabe
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: WidgetScreen(),
+    );
+  }
+}
+
+class WidgetScreen extends StatefulWidget {
+  const WidgetScreen({super.key});
+
+  @override
+  _WidgetScreenState createState() => _WidgetScreenState();
+}
+
+class _WidgetScreenState extends State<WidgetScreen> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState() wurde aufgerufen.');
+  }
+
+  @override
+  void dispose() {
+    print('dispose() wurde aufgerufen.');
+    super.dispose();
+  }
+
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Zahl: $_counter',
+              style: const TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: _incrementCounter,
+              child: const Text('Klick mich!'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
